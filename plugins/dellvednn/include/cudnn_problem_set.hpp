ba52b4d5694444b0bde80bdc3330473ed9a8dc70
qbaula <qdbaula@utexas.edu>
2017-03-16 23:54:03 -0500

    Updated Pooling and Merge

---
+++ plugins/dellvednn/include/cudnn_problem_set.hpp
@@ -0,0 +1,61 @@
+#ifndef DELLVE_CUDNN_PROBLEM_SET_H_
+#define DELLVE_CUDNN_PROBLEM_SET_H_
+
+#include <vector>
+#include <tuple>
+#include <string>
+#include <iostream>
+
+#include "csv.hpp"
+
+class CudnnConvProblemSet {
+private:
+    std::vector<std::tuple<int, int, int, int, int, int, int, int, int, int, int>> problems_;
+
+public:
+    CudnnConvProblemSet(std::string filename) {
+        problems_ = {};
+        io::CSVReader<11> in(filename);
+        in.read_header(io::ignore_extra_column, "w", "h", "c", "n", "k", "r", "s", "pad_w", 
+                                                "pad_h", "wstride", "hstride");
+        int w, h, c, n, k, r, s, pad_w, pad_h, wstride, hstride;
+        while(in.read_row( w, h, c, n, k, r, s, pad_w, pad_h, wstride, hstride)) {
+            problems_.push_back(std::make_tuple(w, h, c, n, k, r, s, pad_w, pad_h, wstride, hstride));
+        }
+    }
+
+    std::tuple<int, int, int, int, int, int, int, int, int, int, int> get(int i) {
+        return problems_[i];
+    }
+
+    int getSize(void) {
+        return problems_.size();
+    }
+};
+
+class CudnnPoolProblemSet {
+private: 
+    std::vector<std::tuple<int, int, int, int, int, int, int, int, int, int>> problems_;
+
+public: 
+   CudnnPoolProblemSet(std::string filename) {
+        problems_ = {};
+        io::CSVReader<10> in(filename);
+        in.read_header(io::ignore_extra_column, "w", "h", "c", "n", "win_h", "win_w", "pad_w", 
+                                                "pad_h", "wstride", "hstride");
+        int w, h, c, n, win_w, win_h, pad_w, pad_h, wstride, hstride;
+        while(in.read_row( w, h, c, n, win_w, win_h, pad_w, pad_h, wstride, hstride)) {
+            problems_.push_back(std::make_tuple(w, h, c, n, win_h, win_w, pad_w, pad_h, wstride, hstride));
+        }
+   } 
+
+   std::tuple<int, int, int, int, int, int, int, int, int, int> get(int i) {
+       return problems_[i];
+   }
+
+   int getSize(void) {
+       return problems_.size();
+   }
+};
+
+#endif // DELLVE_CUDNN_PROBLEM_SET_H_
---
+++ plugins/dellvednn/src/forward_pool.cu
@@ -0,0 +1,22 @@
+#include <string>
+#include <stdio.h>
+
+#include "cudnn_pool_driver.hpp"
+#include "cudnn_problem_set.hpp"
+#include "cli_parser.hpp"
+
+int main(int argc, char *argv[]) {
+    CLIParser options(argc, argv); 
+    CudnnPoolProblemSet problems(options.getProblemSetFile());
+    CudnnPoolDriver driver(CudnnPoolMethod::FORWARD, problems, options.getNumRuns(),
+                            options.getGpus());
+
+    printf("Starting Forward Pool through %s set with %d runs\n", options.getProblemSetFile().c_str(), 
+            options.getNumRuns());
+    for (auto i = 0; i < problems.getSize(); i++) {
+        int time = driver.run(i);
+        printf("Set %d took avg %d us\n", i+1, time);
+    }
+    
+    return 0;
+}
