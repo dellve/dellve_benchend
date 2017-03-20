6cbbe7161383a9082d1c29dc60e02dc5dde851f9
Konstantyn Komarov <komarov.konstant@utexas.edu>
2017-03-20 14:50:39 -0500

    Updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='dellve',
-    version='0.1',
+    version='0.1.2',
     packages=find_packages(),
     include_package_data=True,
     package_data={
