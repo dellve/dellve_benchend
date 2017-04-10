7d5f5f9001e1b90d8fb826303f67f4ff59cf1ada
Kian Baula <qdbaula@utexas.edu>
2017-04-10 09:43:26 -0500

    Implement stop functionality for stress tools

--- plugins/dellvednn_stresstool/StressTools/StressFactory.py
+++ plugins/dellvednn_stresstool/StressTools/StressFactory.py
@@ -1,22 +1,32 @@
-import dellve
-import dellve_cudnn_benchmark as dcb
-import time
 from abc import ABCMeta, abstractmethod
+import time
 
-class StressToolFactory(dellve.Benchmark):
+from dellve import Benchmark, BenchmarkInterrupt
+import dellve_cudnn_benchmark as dcb
+
+class StressToolFactory(Benchmark):
     __metaclass__ = ABCMeta
 
     @abstractmethod
     def get_controller(self): pass
 
     def routine(self):
-        controller = self.get_controller()
-        controller.start(1,100)
+        try:
+            self.controller = self.get_controller()
+            self.controller.start(1,100)
 
-        p = controller.get_progress()
+            while (not self.complete()):
+                self.update_progress()
+                time.sleep(0.5)
 
-        while (p != 1.0):
-            p = controller.get_progress()
-            if p > 0:
-                self.progress = p * 100
-            time.sleep(0.5)
+        except BenchmarkInterrupt:
+            print '\nStopping current stress tool'
+            self.controller.stop()
+
+    def complete(self):
+        return self.controller.get_progress() == 1.0
+
+    def update_progress(self):
+        p = self.controller.get_progress()
+        if (p > 0):
+            self.progress = p * 100
