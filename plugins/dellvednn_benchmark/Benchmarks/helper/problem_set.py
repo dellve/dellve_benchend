e524e799ae57d6494d135b7f19c6dcd59af3958b
Kian Baula <qdbaula@utexas.edu>
2017-04-10 07:33:38 -0500

    Simplify CSV reader by using comma as delim

--- plugins/dellvednn_benchmark/Benchmarks/helper/problem_set.py
+++ plugins/dellvednn_benchmark/Benchmarks/helper/problem_set.py
@@ -4,11 +4,12 @@ import os
 def csv_get_problem_set(name):
     result = []
     name = os.path.join(os.path.dirname(__file__), '../problemsets/' + name)
+
     with open(name, 'rb') as csvfile:
-        reader = csv.reader(csvfile, delimiter=' ', quotechar='|')
-        next(reader, None)  # skip header
+        reader = csv.reader(csvfile, delimiter=',', quotechar='|', skipinitialspace=True)
+        next(reader, None) # Skip header 
         for row in reader:
-            result.append([int(s.strip(',')) for s in filter(None, row)])
+            result.append([int(s) for s in row])
 
     return result
         
