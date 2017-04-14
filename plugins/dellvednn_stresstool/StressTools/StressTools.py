bbe5685c0b506166f79d51cc8dc6ff4f72ddd325
Kian Baula <qdbaula@utexas.edu>
2017-04-14 15:48:50 -0500

    Pooling Stress Tool problem set implemented

--- plugins/dellvednn_stresstool/StressTools/StressTools.py
+++ plugins/dellvednn_stresstool/StressTools/StressTools.py
@@ -39,26 +39,25 @@ class ForwardPoolingStressTool(StressToolFactory):
     name = 'ForwardPoolingStressTool'
 
     def get_controller(self):
-        # TODO: implement
-        print self.memutil
-        n,c,h,w = problem_size.calculate_nchw_pooling_forward(1,self.memutil)
-        print n
-        print c
-        print h
-        print w
-        controller = dcb.pooling_forward(w,h,c,n,10,10,0,0,2,2,"max")
+        win = 3
+        pad = 1
+        stride = 1
+        n,c,h,w = problem_size.calculate_nchw_pooling(1,self.memutil,win,pad,stride)
+
+        controller = dcb.pooling_forward(w, h, c, n, win, win, pad, pad, stride, stride, "max")
         return controller
 
 class BackwardPoolingStressTool(StressToolFactory): 
     name = 'BackwardPoolingStressTool'
 
     def get_controller(self):
-        # TODO: implement
-        print self.memutil
-        n,c,h,w = problem_size.calculate_nchw_pooling_forward(1,self.memutil)
-        print n
-        print c
-        print h
-        print w
-        controller = dcb.pooling_forward(w,h,c,n,10,10,0,0,2,2,"max")
+        win = 3
+        pad = 1
+        stride = 1
+        n,c,h,w = problem_size.calculate_nchw_pooling(1,self.memutil,win,pad,stride)
+
+        controller = dcb.pooling_backward(w, h, c, n, win, win, pad, pad, stride, stride, "max")
         return controller
+
+
+        
--- plugins/dellvednn_stresstool/StressTools/helper/problem_size.py
+++ plugins/dellvednn_stresstool/StressTools/helper/problem_size.py
@@ -1,6 +1,8 @@
 import gpu_info
 import math
 
+__OVERHEAD = 71*1000*1000
+
 def calculate_nchw(mem):
     c = 1
     n = 75
@@ -15,9 +17,8 @@ def calculate_nchw(mem):
 
 def calculate_nchw_forward(device_id, mem_util):
     # TODO: Limit Mem_Util to .5 < mem_util < .95
-    overhead = 71*1000*1000
     mem = int(gpu_info.get_total_mem(device_id) * mem_util)
-    mem -= overhead
+    mem -= __OVERHEAD
     mem /= 8
 
     return calculate_nchw(mem)
@@ -25,9 +26,8 @@ def calculate_nchw_forward(device_id, mem_util):
 def calculate_nchw_activation_backward(device_id, mem_util):
     # TODO: Limit Mem_Util
     # Overhead counted twice so half it from forward
-    overhead = (71*1000*1000)/2
     mem = int((gpu_info.get_total_mem(device_id)/2) * mem_util)
-    mem -= overhead
+    mem -= __OVERHEAD/2
     mem /= 8
 
     return calculate_nchw(mem)
@@ -35,35 +35,20 @@ def calculate_nchw_activation_backward(device_id, mem_util):
 def calculate_nchw_softmax_backward(device_id, mem_util):
     # TODO: Limit Mem_Util
     # Overhead counted roughly 2/3 (calc 2611/1763 =~ 3/2)
-    overhead = ((71*1000*1000)*2)/3
     mem = int(((gpu_info.get_total_mem(device_id)*2)/3) * mem_util)
-    mem -= overhead
+    mem -= (__OVERHEAD*2)/3
     mem /= 8
 
     return calculate_nchw(mem)
 
-# Stuff~
-def add_pooling(mem):
-    dep = 633*1024*1024
-    dep_source = 1848705024
-    dep_f = mem/float(dep_source)
-    dep *= dep_f
-    dep *= (1.5)
-
-    return int(dep) 
-
-# Stuff~
-def calculate_nchw_pooling_forward(device_id, mem_util):
-    overhead = (71*1000*1000)
+def calculate_nchw_pooling(device_id, mem_util, win, pad, stride):
     mem = int(gpu_info.get_total_mem(device_id) * mem_util)
 
-    s = add_pooling(mem)
-    mem += s
-
-    #mem += s
+    mem -= (__OVERHEAD)
     mem /= 8
 
-    return calculate_nchw(mem)
+    n,c,hp,wp = calculate_nchw(mem)
 
-
-     
+    h = ((hp - 1) * stride) + win - 2*pad
+    
+    return n,c,h,h
