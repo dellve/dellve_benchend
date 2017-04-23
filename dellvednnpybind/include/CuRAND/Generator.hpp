46abf380cee14c63d8019d34681292f9297bc4e0
Kian Baula <qdbaula@utexas.edu>
2017-04-22 22:14:17 -0500

    Refactor CuRAND/Generator

--- dellvednnpybind/include/CuRAND/Generator.hpp
+++ dellvednnpybind/include/CuRAND/Generator.hpp
@@ -1,28 +1,58 @@
 #ifndef CURAND_GENERATOR_HPP
 #define CURAND_GENERATOR_HPP
 
+#include <memory>
+
 #include <curand.h>
 #include "Ordering.hpp"
-#include "RAII.hpp"
 #include "RngType.hpp"
 #include "Status.hpp"
 
 namespace CuRAND {
+    class Generator {
+    private:
 
-    Status CreateGenerator(curandGenerator_t *generator) {
-        RngType type = CURAND_RNG_PSEUDO_XORWOW;
-        Ordering order = CURAND_ORDERING_PSEUDO_SEEDED;
+        struct RawGenerator {
+            curandGenerator_t gen;
 
-        checkStatus(curandCreateGenerator(generator, type));
-        checkStatus(curandSetPseudoRandomGeneratorSeed(*generator, 4242ULL));
-        checkStatus(curandSetGeneratorOrdering(*generator, order));
+            RawGenerator(RngType type = CURAND_RNG_PSEUDO_XORWOW) {
+                checkStatus(curandCreateGenerator(&gen, type));
+            }
 
-        return CURAND_STATUS_SUCCESS;
-    }
+            ~RawGenerator() {
+                checkStatus(curandDestroyGenerator(gen));
+            }
 
-    class Generator : 
-        public RAII<curandGenerator_t,
-                    CreateGenerator,
-                    curandDestroyGenerator> {};
+            operator curandGenerator_t () {
+                return gen;
+            }
+        };
+
+        std::shared_ptr<RawGenerator> generatorPtr;
+
+    public:
+        Generator(RngType type = CURAND_RNG_PSEUDO_XORWOW, 
+                  unsigned long long seed = 4242ULL,
+                  Ordering order = CURAND_ORDERING_PSEUDO_SEEDED) :
+                  generatorPtr(std::make_shared<RawGenerator>(type)) {
+            SetPseudoRandomGeneratorSeed(seed);
+            SetGeneratorOrdering(order);
+        }
+
+        void SetPseudoRandomGeneratorSeed(unsigned long long seed) {
+            struct RawGenerator *generator = generatorPtr.get();
+            checkStatus(curandSetPseudoRandomGeneratorSeed(*generator, seed));
+        } 
+
+        void SetGeneratorOrdering(Ordering order) {
+            struct RawGenerator *generator = generatorPtr.get();
+            checkStatus(curandSetGeneratorOrdering(*generator, order));
+        }
+
+        operator curandGenerator_t () {
+            return generatorPtr->gen; 
+        }
+    };
 }
+
 #endif // CURAND_GENERATOR_HPP
